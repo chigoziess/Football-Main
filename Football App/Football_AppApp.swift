61c7591e7a454d3fa74fd14425863b38e598dfeb
Chigozie Sumani <sumanic@bc.edu>
2025-01-20 05:16:38 -0500

    Initial Commit

---
+++ Football App/Football_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Football_AppApp.swift
+//  Football App
+//
+//  Created by Chigozie Sumani on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Football_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
